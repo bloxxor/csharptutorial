3fab4fc4ebe8e5f35e76c690228faccafa2a59db
Bloxxor <f.gabele@bytebenders.de>
2022-11-07 14:48:33 +0100

    Kreisberechnung

--- prjKreisberechnung/Program.cs
+++ prjKreisberechnung/Program.cs
@@ -9,13 +9,12 @@ namespace prjKreisberechnung
         static void Main(string[] args)
         {
 
-            c.WriteLine("Radius eingeben: ");
+            c.WriteLine("Radius eingeben (cm): ");
             double radius = Double.Parse(c.ReadLine());
-            c.WriteLine(radius);
 
-            c.WriteLine("Umfang ist: {0:F2}", Kreisberechnung.berechneUmfang(radius));
-            c.WriteLine("Durchmesser ist: {0:F2}", Kreisberechnung.berechneDurchmesser(radius));
-            c.WriteLine("Fläche ist: {0:F2}", Kreisberechnung.berechneFlaeche(radius));
+            c.WriteLine("Umfang ist: {0:F2}cm", Kreisberechnung.berechneUmfang(radius));
+            c.WriteLine("Durchmesser ist: {0:F2}cm", Kreisberechnung.berechneDurchmesser(radius));
+            c.WriteLine("Fläche ist: {0:F2}cm²", Kreisberechnung.berechneFlaeche(radius));
 
             c.ReadKey();
 
